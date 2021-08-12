c33a205980ccc8d37142a1c634cf7489d8f07aa3
maxim <maxim.bleeker@gmail.com>
2021-08-12 01:24:20 -0400

    Frontend recieves studios successfully

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "studios#index"
+  
+  # resources :studios
   get "/studios", to: "studios#index"
+  get "/studio/:id", to: "studios#show"
 end
